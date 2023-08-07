8c018a12d53babd3656206139a1ca766ad1ae079
Dylan J Dombrowski <d0domb01@louisville.edu>
2023-08-07 12:58:20 -0400

    setup

---
+++ app.js
@@ -0,0 +1,7 @@
+function createGrid(numOfSquares) {
+  for (let rows = 0; rows < numOfSquares; rows++) {
+    for (let columns = 0; columns < numOfSquares; columns++) {
+      document.createElement("div");
+    }
+  }
+}
